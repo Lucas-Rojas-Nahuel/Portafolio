54c255eb1ae0734662c4e77909cb7c437a21fda7
Lucas-Rojas-Nahuel <rolukasro12@gmail.com>
2026-06-24 17:12:08 -0300

    instalando plugin de rendimiento y configurandolo para mejorar el rendimiento de perfomance

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,32 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+// o @vitejs/plugin-react
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    visualizer({ open: true, filename: "bundle-analysis.html" }),
   ],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        // Esta función corta el bloque gigante de node_modules
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            // Ponemos react y react-dom en su propio chunk estratégico
+            if (id.includes("react-dom") || id.includes("react/")) {
+              return "react-core";
+            }
+            // Agrupamos las librerías de iconos en otro chunk separado
+            if (id.includes("react-icons") || id.includes("lucide-react")) {
+              return "icons";
+            }
+            // Todo el resto de dependencias pesadas de terceros irá a vendor
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+});
